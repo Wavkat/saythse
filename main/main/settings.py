3394ec7294d48763886978b27bb1984618a2f5d7
Shavkat <103242944+Wavkat@users.noreply.github.com>
2025-09-10 14:14:22 +0500

    Update settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.Path.abspath(__file__).resolve().parent.parent))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'main.main.urls'
@@ -125,5 +126,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
