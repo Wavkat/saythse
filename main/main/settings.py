e416202d074406bdc3544d51058821678012ad32
Shavkat <103242944+Wavkat@users.noreply.github.com>
2025-09-10 12:53:14 +0500

    Update settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r!=*&g!gg!&qiipg_a0o(npg12-#+(!&1m8tmai4h+d-kqj_%#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["hsesayt.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
